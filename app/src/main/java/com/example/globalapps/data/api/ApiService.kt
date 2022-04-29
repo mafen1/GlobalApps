26cbf3fc4e5d48b8571538e3f155b7ac0294efd0
ok <66274954+mafen1@users.noreply.github.com>
2022-04-30 00:29:05 +0300

    init

--- app/src/main/java/com/example/globalapps/data/api/ApiService.kt
+++ app/src/main/java/com/example/globalapps/data/api/ApiService.kt
@@ -1,4 +1,9 @@
 package com.example.globalapps.data.api
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("/api/activity/")
+    suspend fun fetchActivity(): Response<>
 }
